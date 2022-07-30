77fbca1df3416b173ba91cd37a1739460e78a6b8
Yeongjin Lee <trueway1002@gmail.com>
2022-07-30 22:18:00 +0900

    구구단

---
+++ src/백준/BOJ2739.java
@@ -0,0 +1,14 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class BOJ2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i<10; i++) {
+            System.out.println(N+" * "+i + " = " + N*i);
+        }
+    }
+}
