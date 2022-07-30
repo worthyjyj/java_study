0c7017aba6b2cddf55a5ec5b9c415d8a78ace20c
Yeongjin Lee <trueway1002@gmail.com>
2022-07-30 22:12:30 +0900

    구구단

--- src/New/Main.java
+++ src/New/Main.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-        for (int i =1; i > 0; i++){
-            int A = sc.nextInt(); int B = sc.nextInt();
-            if (A==0 && B==0){
-                break;
-            }
-            System.out.println(A+B);
-            }
+        for (int i = 1; i<10; i++) {
+            System.out.println(N+" * "+i + " = " + N*i);
+        }
     }
 }
