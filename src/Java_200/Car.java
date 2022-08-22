d1e7e0c750c67a5494e577d61a7d4ef40f9e88e5
Yeongjin Lee <trueway1002@gmail.com>
2022-08-22 23:52:35 +0900

    8_3:객체지향프로그래밍_클래스_자동차

---
+++ src/Java_200/Car.java
@@ -0,0 +1,86 @@
+package Java_200;
+
+class Car {
+    private String name; //이름
+    private String number; //번호
+    private int width; //폭
+    private int height; //높이
+    private int length; //길이
+    private double x; //현재 위치의 x 좌표
+    private double y; //현재 위치의 y 좌표
+    private double tankage; //탱크 용량
+    private double fuel; //남은 연료
+    private double sfc; //연비
+
+    Car (String name, String number, int width, int height,
+         int length, double tankage, double fuel, double sfc){
+        this.name=name; this.number=number; this.width=width; this.height=height;
+        this.length=length; this.tankage=tankage;
+        this.sfc=sfc;
+        this.fuel = (fuel>tankage) ? tankage : fuel; //생성자 안에 조건문 생성(삼항연산자로) //this.fuel 지우기
+        x=y=0.0;
+    }
+
+    //값이 변할 수 있는 것들은 메소드로 만들어서 외부클래스 변한 겂을 호출 할 수 있도록 함.
+    double getx(){
+        return x;
+    }
+
+    double gety(){
+        return y;
+    }
+
+    double getfuel(){
+        return fuel;
+    }
+
+    //사양표시하는 메소드
+    void putSpec(){
+        System.out.println("이름: "+name);
+        System.out.println("번호: "+number);
+        System.out.println("전폭: "+width+"mm");
+        System.out.println("전고: "+height+"mm");
+        System.out.println("전장: "+length+"mm");
+        System.out.println("탱크: "+tankage+"리터");
+        System.out.println("연비: "+sfc+"km/리터");
+    }
+
+    //x방향으로 dx, y방향으로 dy이동 가능한지 여부와 이동한 만큼의 좌표 나타내는 메소드
+    boolean move(double dx, double dy){
+        double dist = Math.sqrt(dx*dx+dy*dy); //이동거리
+        double f = dist / sfc; //이동에 필요한 연료
+
+        if (f > fuel)
+            return false;
+        else {
+            fuel=fuel-f;
+            x=dx+x;
+            y=dy+y;
+            return true;
+        }
+    }
+
+    //df 리터 급유
+    void refuel(double df){
+        if (df>0) {
+            fuel = fuel + df;
+        }if (fuel>tankage){
+            fuel=tankage;
+        }
+    }
+}
+
+class CarTester {
+    public static void main(String[] args) {
+        Car k3 = new Car("k3", "서울999-88", 1660,1500,3640, 40.0, 10.0,12);
+
+        k3.putSpec();
+        k3.move(3,3); //첫번째로 움직인 거리
+        k3.move(3,3); //두번째++
+        k3.refuel(45); // 연료 충전. // 연료 최대량 오버해도 자동으로 탱크 최대량으로 맞춰줌.
+        System.out.println("현재 연료량으로 이동이 가능합니까? " + k3.move(100,100));
+        System.out.println("x좌표 이동: " +k3.getx());
+        System.out.println("y좌표 이동: " +k3.gety());
+        System.out.println("현재 연료량 : " + k3.getfuel());
+    }
+}
