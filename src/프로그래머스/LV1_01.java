ab9fddd663301385d4811deafb7dced1cbc18629
worthyjyj <trueway1002@gmail.com>
2023-04-27 23:07:12 +0900

    프로그래머스 자바 연습문제 풀이_

---
+++ src/프로그래머스/LV1_01.java
@@ -0,0 +1,30 @@
+package 프로그래머스;
+
+public class LV1_01 {
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        if((a-b)>0){
+            //a가 더 큰 경우
+            for(long i = b ; i <= a ; i++){
+                answer += i;
+            }
+            return answer;
+        }else{
+            //b가 더 크거나 a,b값이 동일할 경우
+            for(long i = a ; i <= b ; i++){
+                answer += i;
+            }
+            return answer;
+        }
+    }
+
+    public void main(String[] args) {
+
+        LV1_01 instance = new LV1_01();
+
+        long result = solution(3,3);
+        System.out.println(result);
+    }
+}
+
