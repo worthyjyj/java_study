49e8a6ab9da977962620a84f4ea68f57ba59408d
worthyjyj <trueway1002@gmail.com>
2023-05-01 23:45:01 +0900

    프로그래머스 자바_월간 코드 챌린지 시즌1_ 두개 뽑아서 더하기

---
+++ src/프로그래머스/LV1_05.java
@@ -0,0 +1,45 @@
+package 프로그래머스;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class LV1_05 {
+
+    public static int[] solution(int[] numbers) {
+//        ArrayList list = new ArrayList();
+
+
+        // TreeSet을 사용하여 중복제거 및 오름차순이 자동정렬되도록 함
+        Set<Integer> list2 = new TreeSet();
+
+        for (int i=0; i<numbers.length; i++){
+            for(int j=i+1 ; j<numbers.length ; j++){
+                list2.add(numbers[i]+numbers[j]);
+            }
+        }
+
+        System.out.println(list2.toString());
+
+        //배열 생성
+        int[] answer = new int[list2.size()];
+
+        Iterator<Integer> iterSet = list2.iterator();
+        int count = 0;
+        while(iterSet.hasNext()) {
+            answer[count] = iterSet.next();
+            count++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] result = solution(new int[]{2,1,3,4,1});
+        System.out.println(Arrays.toString(result));
+    }
+
+
+
+}
